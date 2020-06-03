0f79fdbc0ec0623c71a76fd9041a063a1dd991b5
HernanRodriguez1 <66162160+HernanRodriguez1@users.noreply.github.com>
2020-06-02 22:21:01 -0500

    Update fuzz0x.py

--- fuzz0x.py
+++ fuzz0x.py
@@ -4,7 +4,9 @@
 import requests
 import urllib2,cookielib
 import sys
+import shodan
 from fake_useragent import UserAgent
+import time
 
 mensaje = '\x1b[1;22m'+"Framework Fuzz0x".capitalize()
 print ('\n'+mensaje.center(100, "=")+'\n'+'''
@@ -21,10 +23,13 @@ Fuzz0x - 2020
 This project was created by Hernan Rodriguez. 
 Copyright under the GPL license'''+'\n')
 
+#----------Obtener nuestra IP Publica---------
+ip = urllib2.urlopen('http://ident.me').read()
+print ('Tu IP Publica es: '+ip)
 
 UserAgent= UserAgent()
-cabecera = {'User-Agent':str(UserAgent.IE)} 
-# "Si deseas puedes modificar el USER-AGENT simplemente reemplaza IE"
+cabecera = {'User-Agent':str(UserAgent.firefox)} 
+#--Si deseas puedes modificar el USER-AGENT simplemente reemplaza IE--
 
 def request(link):
 		try:
@@ -35,38 +40,99 @@ def request(link):
 
 print('Tu User-Agent es: '+str(cabecera)+'\n')
 
-URL = raw_input('Ingrese URL: ')
+#------------------------Banner Grabbing---------------------------
+
+URL = raw_input('Ingrese URL: ')#https://www.example.com o http://wwww.example.com
 banner = urllib2.Request(URL, headers=cabecera)
 
 try:
     pagina = urllib2.urlopen(banner)
-except urllib2.HTTPError, e:
-    print e.fp.info()
+except:
+    print ('[*] Error de conectividad con el servidor web'+'\n')
     sys.exit()
 
 contenido = pagina.info()
 print contenido
 
+Carpeta = raw_input('Ingrese PATH si lo requiere: ') #Elabora el fuzzing en un directorio del servidor
+diccionario = raw_input("Ingrese la Diccionario: ") #Ingrese la ruta de archivo
 
-Carpeta = raw_input('Ingrese PATH si lo requiere: ')
-#No es obligatorio añadir una carpeta
-diccionario = raw_input("Ingrese la Diccionario: ")
-#Añada la ruta de su archivo
+##----------------------Brute Force - FUZZ-------------------------
+
+print '\n'+'=======================RESULTADO========================'+'\n'
 
 try:
 	archivo = open(diccionario,"r")
 
 except:
-		print ("No se encuentro diccionario")
+		print ("No se encuentro diccionario")+'\n'
 		sys.exit()
 
 
-for line in archivo:
-	cadenas = line.strip()
+for linea in archivo:
+	cadenas = linea.strip()
 	directorio_encontrado = URL+Carpeta+'/'+cadenas
 	respuesta = request(directorio_encontrado)
 
 
 	if respuesta:
-		print("[+]Encontrado: "+directorio_encontrado)
+		print("[+] Encontrado: "+directorio_encontrado)		
+		
 
+pass
+print '\n'+"Recopilación de información con shodan"+'\n'
+
+#---------------------API SHODAN-----------------------	
+
+Key = 'WmoZbOoF5wjX2KDPtb442MLAR4FWCVlm' 
+api = shodan.Shodan(Key)
+
+target = raw_input('Ingrese nombre de dominio: ')
+
+dnsResolve = 'https://api.shodan.io/dns/resolve?hostnames=' + target + '&key=' + Key
+
+try:
+    resolved = requests.get(dnsResolve)
+    hostIP = resolved.json()[target]
+    
+    #Obteniendo Banner Grabbing
+    host = api.host(hostIP)
+    print '\n'+"===================IFORMACIÓN SHODAN================"
+    print('''
+
+[!] Direccion IP: {}
+[!] Ciudad: {}
+[!] ISP: {}
+[!] Organización: {}
+[!] Puertos: {}
+[!] Sistema Operativo: {}   
+
+    '''.format(host['ip_str'],host['city'],host['isp'],host['org'],host['ports'],host['os']))
+
+    for item in host['data']:
+        print "Port: %s" % item['port']
+        print "Banner: %s" % item['data']    
+
+    for item in host['vulns']:
+        CVE = item.replace('!','')
+        print '[+] VULNERABILIDAD: %s' % item
+        exploits = api.exploits.search(CVE)
+        for item in exploits['matches']:
+            if item.get('cve')[0] == CVE:
+                print item.get('description')+'\n'
+                time.sleep(1)
+                
+                     
+    file = open('shodan.txt','a+')
+    for elemento in host['data']:
+		lista = elemento.keys()
+		for l in lista:
+		          	 file.write(str(elemento[l]))
+    file.close()
+
+    print "RECOPILACIÓN TERMINADA"+'\n'
+
+except:
+		print ("Error en SHODAN")+'\n'
+		
+        
